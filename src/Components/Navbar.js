da77d025680a626e0e68994100c44a497447b1a3
Mike Chang <mchang95@gmail.com>
2021-09-08 21:55:23 -0700

    changed mb-5 to mb-4 for better spacing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 function Navbar() {
     return (
-        <nav className="navbar navbar-primary bg-primary bg-gradient sticky-top mb-5">
+        <nav className="navbar navbar-primary bg-primary bg-gradient sticky-top mb-4">
             <div>
                 <button className="btn btn-link navbar-brand text-white">CAISO Scraper</button>
             </div>
