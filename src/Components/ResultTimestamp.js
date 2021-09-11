dcd732aa26c64814a76e87120025d9f314cee85a
Mike Chang <mchang95@gmail.com>
2021-09-11 10:31:35 -0700

    created ResultTimestamp component
    - presentational component to display timestamp at bottom of result card
    - has some logic based on if result is live updating or not

---
+++ src/Components/ResultTimestamp.js
@@ -0,0 +1,27 @@
+/** ResultTimestamp: displays timestamp at bottom of result card
+ * 
+ *    props: 
+ *      - report = {
+ *          header: {timestamp: Date object, ...},
+ *          reports: {...},
+ *          request: {...},
+ *        }
+ * 
+ *    Result -> ResultTimestamp
+ */
+
+function ResultTimestamp({ report }) {
+    const timestamp = report.header.timestamp.toLocaleString();
+
+    return (
+        <small className="m-3">
+            {
+                report.header["liveUpdating"]
+                ? <><i className="bi bi-broadcast text-success"></i> Last Updated: {timestamp}</>
+                : <>Report Generated: {timestamp}</>
+            }
+        </small>
+    )
+}
+
+export default ResultTimestamp;
