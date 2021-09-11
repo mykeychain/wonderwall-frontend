c1669b0a38289adb3ab12179cabf0df29d5595eb
Mike Chang <mchang95@gmail.com>
2021-09-11 10:33:02 -0700

    adds ResultTimestamp and primary docstring

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,7 +1,24 @@
 import { useEffect, useState } from "react";
 import ResultChart from "./ResultChart";
 import ResultTable from "./ResultTable";
+import ResultTimestamp from "./ResultTimestamp";
 
+/** Result: stores data for each individual report, renders chart, table,
+ *  timestamp, and manages intervals for live updates
+ * 
+ *    props: 
+ *      - report = {
+ *          header: {}
+ *          reports: {}
+ *          request: {}
+ *        }
+ *      - update = parent function
+ * 
+ *    state: 
+ *      - isAfterFirstUpdate = boolean
+ * 
+ *    CaisoRequest -> Result -> { ResultTable, ResultChart, ResultTimestamp }
+ */
 function Result({ report, update }){
     // const [isLiveUpdating, setIsLiveUpdating] = useState(false);
     const [isAfterFirstUpdate, setIsAfterFirstUpdate] = useState(false);
@@ -9,29 +26,32 @@ function Result({ report, update }){
     useEffect(function setFirstIntervalTimer() {
         const timestamp = report.header.timestamp;
         const firstInterval = ((5 - (timestamp.getMinutes() % 5)) * 60 - timestamp.getSeconds());
-        console.log("FIRST INTERVAL HERE", firstInterval);
-        const firstTimeout = setTimeout(() => {
-            rtmUpdate();
-            setIsAfterFirstUpdate(true);
-        }, firstInterval * 1000);
+        if (report.header["liveUpdating"]) {
+            const firstTimeout = setTimeout(() => {
+                liveUpdate();
+                setIsAfterFirstUpdate(true);
+            }, firstInterval * 1000);
 
-        return function cleanUp() {clearTimeout(firstTimeout)};
+            return function cleanUp() {clearTimeout(firstTimeout)};
+        }
+
+        return;
     }, [])
 
     useEffect(function setRecurringTimer() {
         if (isAfterFirstUpdate) {
             const recurringInterval = setInterval(() => {
                 console.log("INSIDE INTERVAL");
-                rtmUpdate();
+                liveUpdate();
             }, (5 * 60 * 1000));
     
             return function cleanUp() {clearInterval(recurringInterval)};
         }
 
-        return null;
+        return;
     }, [isAfterFirstUpdate])
 
-    async function rtmUpdate() {
+    async function liveUpdate() {
         await update(report.request, report.header.reportId);
     }
 
@@ -42,9 +62,7 @@ function Result({ report, update }){
             </h3>
             <ResultChart report={report}/>
             <ResultTable report={report} />
-            <small className="m-3">
-                <i className="bi bi-broadcast text-success"></i> Report generated: {report.header.timestamp.toLocaleString()}
-            </small>
+            <ResultTimestamp report={report} />
         </div>
     )
 }
