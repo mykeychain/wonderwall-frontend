0c1535bc3be41d10c709a251bfb7f91dfe3ef8e8
Mike Chang <mchang95@gmail.com>
2021-09-17 13:13:29 -0700

    started customized readme and cleaned up unnecessary files

--- src/api.js
+++ src/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/CAISO";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/";
+
 
 /** ScraperApi: class with static methods to interact with backend API */
 class ScraperApi {
   static async getData(formData) {
     try {
-      const resp = await axios.post(BASE_URL, {data: formData});
+      const resp = await axios.post(BASE_URL+"api/CAISO", {data: formData});
       console.log("NO ERROR HERE", resp)
       return resp.data;
     } catch(err) {
